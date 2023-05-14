7098faed68c7869972778e3cb3b9711fa296cbaf
出其东门 <8959823+xia-cha@user.noreply.gitee.com>
2023-05-14 10:07:34 +0800

    feat: 项目列表新增用leader_id进行筛选

--- router_handler/project.js
+++ router_handler/project.js
@@ -2,17 +2,22 @@ const db = require('../db/index')
 const { connectToDatabase } = require('../db/index')
 //获取项目列表
 exports.getProjects = async (req, res) => {
-  //查出当前用户身份
+  //查出当前用户身份 1.通过队员身份 2.通过领导身份
   const db = await connectToDatabase()
   const { user_id } = req
   const { project_name, leader_id } = req.query
-  console.log('后续通过-leaderid进行筛选--', leader_id)
+  console.log(leader_id)
   let sql = `select * from projects where 
-  project_id in (select project_id from project_visibility where team_id in (select team_id from team_members where user_id = ?))`
+  project_id in (select project_id from project_visibility where team_id in (select team_id from team_members where user_id = ${user_id})) or leader_id = ${user_id}`
   if (project_name) {
     sql += ` and project_name like "%${project_name}%"`
   }
-  const [projects] = await db.query(sql, [user_id])
+  let sql_leader = null
+  if (leader_id) {
+    sql_leader = `select * from (${sql}) as t where t.leader_id = ${leader_id}`
+  }
+  console.log(sql)
+  const [projects] = await db.query(leader_id ? sql_leader : sql)
   res.ssend(projects)
   await db.end()
 }
